af353862544d41677dab656b65b7fc3eca2da5dd
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2024-09-25 10:36:47 -0600

    [AGREGA] un comentario del profe

--- boceto_1.1/boceto_1.1/ViewController.swift
+++ boceto_1.1/boceto_1.1/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    var cita_para_enviar: Cita = Cita(quien_lo_dijo: "Creeper", que_dijo: "Tssseñor")
+    var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
 
     override func viewDidLoad() {
+        citas_disponibles.generar_citas_falsas()
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
 
-
+    
+    @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCitas? {
+        return ControladorPantallaCitas(cita_para_citar: citas_disponibles.obtener_cita_aleatoria()
+                                        , coder: coder)
+    }
 }
-
