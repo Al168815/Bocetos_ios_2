3f6d7381b73887df23941bc906c1f250da2b9be2
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2024-09-25 10:31:59 -0600

    funcional sin estilo

---
+++ boceto_1.1/boceto_1.1/controlador/generador_de_citas.swift
@@ -0,0 +1,8 @@
+//
+//  generador_de_citas.swift
+//  boceto_1.1
+//
+//  Created by alumno on 9/25/24.
+//
+
+import Foundation
---
+++ boceto_1.1/boceto_1.1/modelo/Cita.swift
@@ -0,0 +1,8 @@
+//
+//  Cita.swift
+//  boceto_1.1
+//
+//  Created by alumno on 9/25/24.
+//
+
+import Foundation
