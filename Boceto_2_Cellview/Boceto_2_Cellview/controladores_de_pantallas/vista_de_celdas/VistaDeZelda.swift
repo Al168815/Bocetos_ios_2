beb6a0f221e13ae7a2c091cf7e0b703f57f67ba0
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2024-10-07 10:26:17 -0600

    Primera parte Zeldas

---
+++ Boceto_2_Cellview/Boceto_2_Cellview/controladores_de_pantallas/vista_de_celdas/VistaDeZelda.swift
@@ -0,0 +1,14 @@
+//
+//  VistaDeZeldaCollectionViewCell.swift
+//  Boceto_2_Cellview
+//
+//  Created by alumno on 10/7/24.
+//
+
+import UIKit
+
+class VistaDeZelda: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var etiqueta: UILabel!
+}
